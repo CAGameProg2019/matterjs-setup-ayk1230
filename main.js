57a4b50eb88410b4a9e64254cd0673e7de944f73
Ashley <ashleykim@MacBook-Pro.local>
2019-05-16 11:58:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,18 +6,28 @@ let Engine = Matter.Engine,
     Composites = Matter.Composites;
 let engine = Engine.create();
 let render = Render.create({
+    options: {
+        width: 800,
+        height: 600,
+        wireframes:true
+    },
     element: document.body,
     engine: engine
 });
 Engine.run(engine);
 Render.run(render);
- 
+
 let ball=Bodies.circle(200, 10, 40);
+let ball2=Bodies.circle(500, 10, 40);
+let square = Bodies.rectangle(340,10,30,30);
 let floor=Bodies.trapezoid(340,300,500,100,.9,{isStatic: true});
+let underfloor = Bodies.polygon(100,430,3,50);
+let rect =Bodies.rectangle(200,200,100,40);
+let rect2 =Bodies.rectangle(500,200,100,40,{isStatic: true});
 let myCar=Composites.car(390, 0, 100, 30, 40);
-let myCradle=Composites.newtonsCradle(600, 100, 5, 10, 160);
-World.add(engine.world, [ball,floor,myCar,myCradle]);
- 
+let myCradle=Composites.newtonsCradle(600, 100, 7, 10, 160);
+World.add(engine.world, [ball,ball2,square,underfloor,floor,rect,rect2,myCar,myCradle]);
+
 let world = engine.world;
 let Mouse= Matter.Mouse;
 let MouseConstraint=Matter.MouseConstraint;
