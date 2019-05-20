0314a730f6ec28c7e8b19870581d4b7f64b030d1
Ashley <ashleykim@MacBook-Pro.local>
2019-05-20 10:57:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let render = Render.create({
     options: {
         width: 800,
         height: 600,
-        wireframes:true
+        wireframes:false
     },
     element: document.body,
     engine: engine
@@ -27,6 +27,7 @@ Render.run(render);
 
 function box(x, y) {
     return Bodies.rectangle(x, y, 50, 50, {
+        isStatic: true,
         density: 0.002,
         friction: .4,
         render: {
@@ -37,6 +38,34 @@ function box(x, y) {
     });
 }
 
-let ball=Bodies.circle(200, 10, 40);
-let boxes= Composites.stack(400,350, 5, 2, 0, 0, box);
-World.add(engine.world, [ball, box]);
+function makeframe(x, y) {
+    return Bodies.rectangle(x, y, 50, 50, {
+        isStatic: true,
+        density: 0.002,
+        friction: .4,
+        render: {
+            fillStyle: 'gray',
+            strokeStyle: 'black',
+            lineWidth: 5
+        }
+    });
+}
+
+let ball=Bodies.circle(600, 10, 40);
+let boxes= Composites.stack(550,350, 8, 2, 0, 0, box);
+let tri = Bodies.polygon(650,20,3,50);
+let frameBot = Composites.stack(0,500, 8, 1, 0, 0, makeframe);
+let frameLeft = Composites.stack(-2,97, 1, 8, 0, 0, makeframe);
+let square = Bodies.rectangle(650,10,30,30);
+let bigSqu = Bodies.rectangle(400,10,60,30);
+let car=Composites.car(200, 0, 100, 30, 40);
+let trap = Bodies.trapezoid(250,100,400,50,.9);
+World.add(engine.world, [ball, boxes, tri, frameBot, frameLeft, square, car, trap, bigSqu]);
+
+
+
+
+let mouse = Mouse.create(render.canvas);
+let mouseConstraint = MouseConstraint.create(engine, {mouse: mouse});
+World.add(world, mouseConstraint);
+render.mouse = mouse;
