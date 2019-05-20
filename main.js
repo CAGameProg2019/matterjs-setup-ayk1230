5d27c5d4d833b6091e27d733948ad3c53d6c79b9
Ashley <ashleykim@MacBook-Pro.local>
2019-05-20 11:31:20 -0400

    Update main.js

--- main.js
+++ main.js
@@ -52,15 +52,19 @@ function makeframe(x, y) {
 }
 
 let ball=Bodies.circle(600, 10, 40);
-let boxes= Composites.stack(550,350, 8, 2, 0, 0, box);
-let tri = Bodies.polygon(650,20,3,50);
-let frameBot = Composites.stack(0,500, 8, 1, 0, 0, makeframe);
-let frameLeft = Composites.stack(-2,97, 1, 8, 0, 0, makeframe);
+let boxes= Composites.stack(550,100, 8, 2, 0, 0, box);
+let tri = Bodies.polygon(400,470,3,50, {isStatic: true});
+Matter.Body.rotate(tri,-.5);
+let frameBot = Composites.stack(0,500, 16, 1, 0, 0, makeframe);
+let frameLeft = Composites.stack(-2,50, 1, 9, 0, 0, makeframe);
+let frameTop = Composites.stack(0,0, 10, 1, 0, 0, makeframe);
 let square = Bodies.rectangle(650,10,30,30);
-let bigSqu = Bodies.rectangle(400,10,60,30);
-let car=Composites.car(200, 0, 100, 30, 40);
-let trap = Bodies.trapezoid(250,100,400,50,.9);
-World.add(engine.world, [ball, boxes, tri, frameBot, frameLeft, square, car, trap, bigSqu]);
+let bar = Bodies.rectangle(400,400,500,20);
+let circle = Bodies.circle(450, 300, 10);
+let bigSqu = Bodies.rectangle(400,40,60,30);
+let car=Composites.car(200, 30, 100, 30, 40);
+let trap = Bodies.trapezoid(250,100,400,10,2);
+World.add(engine.world, [ball, boxes, tri, frameBot,circle, frameLeft, square, bar, car, bigSqu, frameTop]);
 
 
 
